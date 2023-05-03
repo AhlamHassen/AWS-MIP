a114deabae99ded80240c0a8a32390bdb70850f6
LouisMYDAS <120269622+LouisMYDAS@users.noreply.github.com>
2023-05-03 22:52:09 +1000

    ControllerCustoemr(New)
    updated version

--- API/API/Controllers/CustomerController.cs
+++ API/API/Controllers/CustomerController.cs
@@ -11,35 +11,80 @@ namespace API.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
-        private readonly string _connectionString;
+        private readonly ICustomerRepository _customerRepository;
 
-        public CustomerController(IConfiguration configuration)
+        public CustomerController(ICustomerRepository costumerRepository)
         {
-            _connectionString = configuration.GetConnectionString("MyDbConnection");
+            _customerRepository = costumerRepository;
         }
 
         [HttpGet]
-        public IEnumerable<Customer> Get()
+        public IActionResult GetAll()
         {
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                return connection.Query<Customer>("SELCET * FROM Customer");
-            }
+            var customers = _customerRepository.GetAllAsync();
+            return Ok(customers);
         }
 
         [HttpGet("{id")]
-        public Customer GetById(int id) 
+        public async Task<IActionResult> GetById(int id) 
         {
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                return connection.QuerySingleOrDefault<Customer>("SELECT * FROM Customer WHERE Id = @Id", new {Id = id});
-            }
+            var customer = await _customerRepository.GetAsync(id);
+            return Ok(customer);
         }
 
         [HttpPost]
-        public void Create(Customer customer)
+        public async Task<IActionResult> Create(Customer customer) 
         {
-            using (var connection = new SqlConnection(_connectionString))
+            try
+            {
+                await _customerRepository.CreateAsync(customer);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.ToString);
+                return BadRequest(ex.Message);
+            }
+            return Ok(customer);
+            // return CreatedAtAction(nameof(GetById), new {id=customer.Id},customer);
+        }
+
+        [HttpPut("{id")]
+        public async Task<IActionResult> Update([FromBody] Customer customer) 
+        {
+            try
+            {
+                await _customerRepository.UpdateAsync(customer);
+            }
+            catch(Exception ex)
+            {
+                Console.Error.WriteLine(ex.ToString());
+                return BadRequest();
+            }
+            return Ok(customer);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Customer c = await _customerRepository.GetAsync(id);
+            try
+            {
+                await _customerRepository.DeleteAsync(c);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.ToString());
+                return BadRequest();
+            }
+
+            return Ok(c);
+        }
+        /*[HttpPost]
+        public async Task<IActionResult>
+        {
+            var customer = await _customerRepository.CreateAsync(id);
+            return Ok(customer);
+            /*using (var connection = new SqlConnection(_customerRepository))
             {
                 connection.Execute("INSERT INTO Customer (Name, Email) VALUES (@Name, @Email)", customer);
             }
@@ -52,15 +97,6 @@ namespace API.Controllers
             {
                 connection.Execute("UPDATE Customer SET Name = @Name, Email = @Email WHERE Id = @Id", customer);
             }
-        }
-
-        [HttpDelete("{id}")]
-        public void Delete(int id) 
-        {
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                connection.Execute("DELETE FROM Customer WHERE Id = @Id", new { id = id});
-            }
-        }
+        }*/
     }
 }
