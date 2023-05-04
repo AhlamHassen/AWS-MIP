49af99ddb71e67d2d536e20ab7f76d07d03df8fc
Rey <dyfust@gmail.com>
2023-05-04 22:19:03 +1000

    Account entity and repository interface

---
+++ API/API/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models
+{
+    public class Account
+    {
+        public int Id { get; init; }
+        public int CustomerId { get; init; }
+        public string AccountType { get; init; }
+        public decimal Balance { get; init; }
+    }
+}
---
+++ API/API/Repositories/IAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿using API.Models;
+
+namespace API.Repositories
+{
+    public interface IAccountRepository
+    {
+        Task<Account> GetAccountsByCustomerAsync(int customerId);
+        Task CreateAccount(Account account);
+        Task UpdateAccount(Account account);
+        Task DeleteAccount(int accountId);
+    }
+}
